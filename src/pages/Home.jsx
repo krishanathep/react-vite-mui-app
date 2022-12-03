32e7df506b34d610cc635df1e005e4d257eaa8e2
krishanathep <krishanathep@gmail.com>
2022-12-03 21:58:13 +0700

    first deploy

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React, { useMemo, useState, useEffect } from "react";
+import MaterialReactTable from "material-react-table";
+
+export default function Home() {
+  const [users, setUsers] = useState([]);
+
+  async function getData() {
+    await fetch("https://www.melivecode.com/api/users")
+      .then((res) => res.json())
+      .then((res) => setUsers(res));
+      console.log(users)
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "id",
+        header: "ID",
+      },
+      {
+        accessorKey: "fname",
+        header: "First Name",
+      },
+      {
+        accessorKey: "lname",
+        header: "Last Name",
+      },
+      {
+        accessorKey: "username",
+        header: "User Name",
+      },
+      {
+        accessorKey: "avatar",
+        header: "Avatar",
+      },
+    ],
+    []
+  );
+
+  return <MaterialReactTable columns={columns} data={users} />;
+}
